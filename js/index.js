52e12362f749b6ad647eaaa104f296adc741aba9
GrossoMaximiliano <maxigrooso@gmail.com>
2024-10-13 23:54:09 -0300

    mejoras, responsive y mas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 
+function toggleMenu(){
+    let mobile = document.getElementById("atajos_mobile");
+    let close_menu = document.getElementById("close_menu");
+    
+    if ( ['0', '0px', ''].includes(mobile.style.maxHeight) )
+    {
+        mobile.style.maxHeight = "100vh";
+        close_menu.style.fontSize = "30px";
+    }
+    else {
+        mobile.style.maxHeight = "0px";
+        close_menu.style.fontSize = "0";
+    }
+}
+
+function closeMenu(){
+    let mobile = document.getElementById("atajos_mobile");
+    let close_menu = document.getElementById("close_menu");
+    mobile.style.maxHeight = "0px";
+    close_menu.style.fontSize = "0";
+}
+
 addEventListener("scroll", (event) => {
     if ( window.scrollY > 100 ) {
         document.getElementById("navinfo").style.maxHeight = "0px";
